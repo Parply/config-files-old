f68074e0ea453bf6662d04978b4be43dbcf3e0cd
Alexander John Pinches <42705013+Parply@users.noreply.github.com>
2020-06-27 02:56:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,55 +1,13 @@
 #!/usr/bin/bash
 shopt -s expand_aliases
-
-cd ..
-USERINVOKING=$(basename $PWD)
-cd config-files
 alias drawline='tput setaf 2; printf "%*s\n" "${COLUMNS:-$(tput cols)}" "" | tr " " - ;tput setaf 7'
 
-drawline
-tput setaf 6;echo "Starting Instillation of Terminal..."
-drawline
 
-drawline
-tput setaf 6;echo "Adding Progress Bar and Colours to apt"
-drawline
-
-cp $PWD/99progressbar  /etc/apt/apt.conf.d
-chmod 644 /etc/apt/apt.conf.d/99progressbar
-
-drawline
-tput setaf 6;echo "Performing update and upgrade"
-drawline
-
-apt update -y && apt upgrade -y
-
-drawline
-tput setaf 6;echo "Installing Required Packages and Adding Git Colours"; echo "curl openssh-server openssh-client xclip wget git perl tilix tmux neofetch fonts-firacode dconf-cli ruby ruby-dev zsh python3 python3-pip python3-dev" 
-drawline
-
-apt install -y curl openssh-server openssh-client xclip wget git perl tilix tmux neofetch fonts-firacode dconf-cli ruby ruby-dev zsh python3 python3-pip python3-dev
-git config --global color.ui auto
-
-drawline
-tput setaf 6;echo "Installing Anaconda"
-drawline
-cd ..
-wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh -O anaconda.sh
-bash ./anaconda.sh -b -p .anaconda3
-
-drawline
-tput setaf 6;echo "Setting Default Shell to Z-Shell and Installing Oh My Zsh"
-drawline
-
-chsh -s /usr/bin/zsh root
-chsh -s /usr/bin/zsh $USERINVOKING
-
-touch .zshrc
-touch ~/.zshrc
+if [-f continue_install.txt]; then 
  
-#runuser -l $USERINVOKING 'curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh'
-runuser -l $USERINVOKING 'wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O install.sh'
-runuser -l $USERINVOKING 'export CHSH="no"; export RUNZSH="no"; sh ./install.sh'
+curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O install.sh
+export CHSH="no"; export RUNZSH="no"; sh ./install.sh
 
  
  
@@ -73,8 +31,8 @@ drawline
 #dconf write "/com/gexperts/Tilix/profiles/$(dconf list /com/gexperts/Tilix/profiles/)font" 'Fira Coda weight=453 12'
 #dconf write "/com/gexperts/Tilix/profiles/$(dconf list /com/gexperts/Tilix/profiles/)background-transparency-percent" 21
 
-runuser -l $USERINVOKING -c "export DISPLAY=:0.0; xhost +; dconf load /org/gnome/terminal/ < config-files/gnome-terminal.conf; xhost -"
-runuser -l $USERINVOKING -c "export DISPLAY=:0.0; xhost +; dconf load /com/gexpert/ < config-files/tilix-terminal.conf; xhost -"
+export DISPLAY=:0.0; xhost +; dconf load /org/gnome/terminal/ < config-files/gnome-terminal.conf; xhost -
+export DISPLAY=:0.0; xhost +; dconf load /com/gexpert/ < config-files/tilix-terminal.conf; xhost -
 
 
 drawline
@@ -83,19 +41,6 @@ drawline
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $PWD/.oh-my-zsh/custom/themes/powerlevel10k
 
-drawline
-tput setaf 6;echo "Installing Colourful List Files"
-drawline
-
-gem install colorls
-
-apt update -y && apt upgrade -y
-
-drawline
-tput setaf 6;echo "Installing Glances System Monitoring Tool and Addons"
-drawline
-
-pip3 install psutil 'glances[action,browser,cloud,cpuinfo,docker,export,folders,gpu,graph,ip,raid,snmp,web,wifi]' 
 
 drawline
 tput setaf 6;echo "Installing Powerlines for TMUX"
@@ -139,6 +84,71 @@ tput setaf 6;echo "DONE!!! Close and Open Tilix"
 drawline
 drawline
 
+else
+
+cd ..
+USERINVOKING=$(basename $PWD)
+cd config-files
 
 
+drawline
+tput setaf 6;echo "Starting Instillation of Terminal..."
+drawline
+
+drawline
+tput setaf 6;echo "Adding Progress Bar and Colours to apt"
+drawline
+
+cp $PWD/99progressbar  /etc/apt/apt.conf.d
+chmod 644 /etc/apt/apt.conf.d/99progressbar
+
+drawline
+tput setaf 6;echo "Performing update and upgrade"
+drawline
+
+apt update -y && apt upgrade -y
+
+drawline
+tput setaf 6;echo "Installing Required Packages and Adding Git Colours"; echo "curl openssh-server openssh-client xclip wget git perl tilix tmux neofetch fonts-firacode dconf-cli ruby ruby-dev zsh python3 python3-pip python3-dev" 
+drawline
+
+apt install -y curl openssh-server openssh-client xclip wget git perl tilix tmux neofetch fonts-firacode dconf-cli ruby ruby-dev zsh python3 python3-pip python3-dev
+git config --global color.ui auto
+
+drawline
+tput setaf 6;echo "Installing Anaconda"
+drawline
+cd ..
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh -O anaconda.sh
+bash ./anaconda.sh -b -p .anaconda3
+
+drawline
+tput setaf 6;echo "Installing Colourful List Files"
+drawline
+
+gem install colorls
+
+apt update -y && apt upgrade -y
+
+drawline
+tput setaf 6;echo "Installing Glances System Monitoring Tool and Addons"
+drawline
+
+pip3 install psutil 'glances[action,browser,cloud,cpuinfo,docker,export,folders,gpu,graph,ip,raid,snmp,web,wifi]' 
+
+drawline
+tput setaf 6;echo "Setting Default Shell to Z-Shell and Installing Oh My Zsh"
+drawline
+
+chsh -s /usr/bin/zsh root
+chsh -s /usr/bin/zsh $USERINVOKING
+
+touch .zshrc
+touch ~/.zshrc
+
+echo "touch continue_install.txt; zsh ./config-files/install.sh" >> .zshrc
+
+runuser -l $USERINVOKING -c "zsh"
+
+fi
 
